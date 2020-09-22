90114bbe89efd24b3e57a1f423cf53d0d3ff22fa
sonmic <49422933+sonmic@users.noreply.github.com>
2020-09-21 21:41:00 -0500

    Automatically determine live theme id.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-ARGS="--password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL --dir=$THEME_PATH"
+ARGS="--password=$SHOPIFY_PASSWORD --store=$SHOPIFY_STORE_URL"
 
-if [ -n "$SHOPIFY_THEME_ID" ]; then
-    ARGS="$ARGS --themeid=$SHOPIFY_THEME_ID"
+if [ -z "$SHOPIFY_THEME_ID" ]; then
+    SHOPIFY_THEME_ID="`theme get --list $ARGS | grep -oP '(?<=\[)\d+(?=\]\[live\]\s+)'`"
+    echo "Using live theme id: $SHOPIFY_THEME_ID"
 fi
+
+ARGS="$ARGS --dir=$THEME_PATH --themeid=$SHOPIFY_THEME_ID"
 
 theme download $ARGS $INPUT_ARGS
